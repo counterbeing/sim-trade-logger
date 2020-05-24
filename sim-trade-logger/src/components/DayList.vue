b670d999aa9e0e1288180407a260ae9f3e0298ca
Cory Logan <he@corylogan.com>
2020-05-24 09:52:14 -0400

    funny error from vue

---
+++ sim-trade-logger/src/components/DayList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <pre>{{ days }}</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import raw from "raw_loader!./raw.txt";
+
+@Component
+export default class DayList extends Vue {
+  // @Prop() private msg!: string;
+
+  private raw: string = raw;
+
+  get days() {
+    console.log(this);
+    return raw;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
